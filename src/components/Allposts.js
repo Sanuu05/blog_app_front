4aa69835da813fd22f9dd41758b51e6584c99c7b
Shantanu Saha <sansaha0505@gmail.com>
2022-06-05 01:45:22 +0530

    first commit

--- src/components/Allposts.js
+++ src/components/Allposts.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useEffect } from 'react'
+import { Container } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import { getpostData } from '../action/main'
 import Posts from './Posts'
@@ -13,11 +14,14 @@ function Allposts({update}) {
     const posts = useSelector(state => state.getposts.user)
   return (
     <div className='mtop'>
-         {
+      <Container>
+      {
             posts?.map((val,i)=>{
               return  <Posts val={val} res={update} />
             })
           }
+      </Container>
+        
         
     </div>
   )
